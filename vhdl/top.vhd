b009d67015578d7a779eb70fa4596a55480c8bc0
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-09-30 11:22:12 -0600

    VHDL skeleton builds now, assuming tools and libraries are set correctly

---
+++ vhdl/top.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity top is
+    generic (
+        PLACEHOLDER     : boolean   := true
+    );
+    port (
+        clk     : in    std_logic;
+        rst     : in    std_logic
+    );
+
+end entity top;
+
+architecture placeholder of top is
+
+begin
+
+end architecture placeholder;
